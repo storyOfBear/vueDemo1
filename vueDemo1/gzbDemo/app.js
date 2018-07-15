3926507e9affdc099741ec2970fa7f22482210ee
郭家辉 <guojiahui@guojiahuideMacBook-Pro.local>
2018-07-16 01:06:04 +0800

    add

---
+++ vueDemo1/gzbDemo/app.js
@@ -0,0 +1,17 @@
+;(function(Vue) {
+    const dataArray = [
+        {
+            title:'吃的什么饭',
+            subTitle:'火锅'
+        }
+    ];
+    new Vue({
+        el:'',
+        data: {
+            dataArray,
+        },
+        methods: {
+
+        },
+    })
+})(Vue)
